72021d33685b5ea8712c92ee0b8c965aca6d15b7
Igor Tieso <igor.tieso@medcof.tech>
2026-02-11 02:26:40 -0300

    docs(mapping): add XML documentation to OrderProfile
    - Add XML documentation for AutoMapper profile
    - Explicit mapping for Items collection

--- OrderManager.Application/Mappings/OrderProfile.cs
+++ OrderManager.Application/Mappings/OrderProfile.cs
@@ -4,12 +4,16 @@ using OrderManager.Domain.Entities;
 
 namespace OrderManager.Application.Mappings;
 
+/// <summary>
+/// AutoMapper profile for Order entity mappings.
+/// </summary>
 public class OrderProfile : Profile
 {
     public OrderProfile()
     {
         CreateMap<Order, OrderResponse>()
-            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
 
         CreateMap<OrderItem, OrderItemResponse>();
     }
